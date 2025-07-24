68235d22a2a82a0336d961869aaf521413672f56
amir <amirtaki623@gmail.com>
2025-07-24 22:37:23 +0330

    add styles tailwind  at project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import logo from "../images/logo.png"
+import { navItems } from "../constants";
 const Navbar = () =>{
     return(
         <>
-            <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-                <div className="container px-4 mx-auto relative text-sm">
+            <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80 ">
+                <div className="container px-4 mx-auto relative text-sm flex sm:justify-center justify-start">
                     <div className="flex jusitfy-center items-center">
                         <div className="flex items-center flex-shrink-0"></div>
                         <img src={logo} alt="logo" className="h-10 w-10 mr-2"/>
                         <span className="text-xl tracking-tight">
                             VirtualR
                         </span>
+
+                        <ul className="hidden lg:flex ml-14 space-x-12">
+                            {navItems.map((item, index) =>(
+                                <li key = {index}>
+                                    <a href={item.href}>{item.label}</a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <ul className="hidden lg:flex ml-14 space-x-12">
-                        
-                    </ul>
+              
                 </div>
             </nav>
         </>
