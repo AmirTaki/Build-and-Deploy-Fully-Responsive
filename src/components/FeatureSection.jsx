528c4148b8cccce0dedd2d4dcecf4a4f9e3bf563
amir <amirtaki623@gmail.com>
2025-07-24 23:57:13 +0330

    add div span at project

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,7 +1,11 @@
 const FeatureSection = () => {
     return (
         <>
-            FeatureSection
+            <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+                <div className="text-center">
+                    <span className="bg-neutral-900 text-orange-500"></span>
+                </div>
+            </div>
         </>
     )
 }
