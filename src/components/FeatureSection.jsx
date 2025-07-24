a9c38849c1f24ac8393a80fbd27e0cc4be3a09ad
amir <amirtaki623@gmail.com>
2025-07-24 23:58:34 +0330

    add span fetures at project

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -3,7 +3,9 @@ const FeatureSection = () => {
         <>
             <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
                 <div className="text-center">
-                    <span className="bg-neutral-900 text-orange-500"></span>
+                    <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm  font-medium px-2 py-1 uppercase">
+                        FeatureSection
+                    </span>
                 </div>
             </div>
         </>
