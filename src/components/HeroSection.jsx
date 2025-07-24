338d5bc33974909917a326778e18ce52ef81980d
amir <amirtaki623@gmail.com>
2025-07-24 23:41:56 +0330

    add video 1 at project

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -25,9 +25,14 @@ const HeroSection  = () => {
                         Documentation
                     </a>
                 </div>
+                <div className="flex mt-10 justify-center">
+                    <video  autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+                        <source src = {video1} type = "video/mp4"></source>
+                    </video>
+                </div>
             </div>
             <video src={video2}></video>
-            <video src={video1}></video>
+            
         </>
     )
 }
