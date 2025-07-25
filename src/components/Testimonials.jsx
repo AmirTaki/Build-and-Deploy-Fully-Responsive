8858ff8a1c66b43bac583019ee056a2c37269cb4
amir <amirtaki623@gmail.com>
2025-07-25 22:52:07 +0330

    add testimonial>text at component at project

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,20 @@
 import { testimonials } from "../constants";
 
-testimonials
+
 const Testimonials = () => {
     return(
         <>
             <div className = "mt-20 tracking-wide">
                 <h2 className = "text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">What people are saying</h2>
+                <div className = "flex flex-wrap justify-center">
+                    {testimonials.map((testimonial, index) => (
+                        <div key = {index} className = "w-full sm:w-1/2 lg:2-1/3 px-4 py-2">
+                            <div className = "bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
+                                <p>{testimonial.text}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     )
