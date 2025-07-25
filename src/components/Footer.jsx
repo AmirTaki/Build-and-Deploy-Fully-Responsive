20deafc82dffc18312afae1ee787774e55ba3287
amir <amirtaki623@gmail.com>
2025-07-25 23:15:45 +0330

    add resourcesLinks completet to footer js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,13 @@ const Footer = () => {
                 <div className = "grid gird-cols-2 lg:gird-cols-3 gap-4">
                     <div>
                         <h3 className="text-md font-semibold mb-4">Resources</h3>
+                        <ul className="space-y-2">
+                            {resourcesLinks.map((link, index) => (
+                                <li key = {index}>
+                                    <a className="text-neutral-300 hover:text-white" href={link.href}>{link.text}</a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </footer>
