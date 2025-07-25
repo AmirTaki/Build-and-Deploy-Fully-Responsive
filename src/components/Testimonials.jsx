b1db120f426eca4334ceb8ea0e09c8d6fe69eca1
amir <amirtaki623@gmail.com>
2025-07-25 22:47:15 +0330

    add title at Testimonials Components at project

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,12 @@
+import { testimonials } from "../constants";
+
+testimonials
 const Testimonials = () => {
     return(
         <>
-        Testimonials
+            <div className = "mt-20 tracking-wide">
+                <h2 className = "text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">What people are saying</h2>
+            </div>
         </>
     )
 }
