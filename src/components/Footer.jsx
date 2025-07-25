d6aefc77fdbfefb281e3ad7d4625a89edbfaf3ad
amir <amirtaki623@gmail.com>
2025-07-25 23:13:07 +0330

    add resouces add footer project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ const Footer = () => {
     return(
         <>
             <footer className = "mt-20 border-t py-10 border-neutral-700">
-
+                <div className = "grid gird-cols-2 lg:gird-cols-3 gap-4">
+                    <div>
+                        <h3 className="text-md font-semibold mb-4">Resources</h3>
+                    </div>
+                </div>
             </footer>
         </>
     )  
